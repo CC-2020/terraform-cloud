23540e10dc4403894166d49770492a1fb07e46ac
Ubuntu <ubuntu@ip-172-31-95-12.ec2.internal>
2020-03-24 17:25:10 +0000

    nuevas keys

--- provider.tf
+++ provider.tf
@@ -1,6 +1,6 @@
 provider "aws" {
   region     = "us-east-1"
-  access_key = "ASIASA7D5YWLSEGG3UZ6"
-  secret_key = "ZRbQFLYEYWICpiVEHfoDylZJkegfN0bKQdFfZ/G4"
-  token      = "FwoGZXIvYXdzEHEaDGem8obr+SZwlQPU2yK9AeG/+2rd6Ol/u88hhHOcfVsZl6ch2WFmp4W+cMboaZmVgx2i12APAE1JZ/Ad0+IFDcDuVEEFESM976psKGM51e5awR4tjcOR5np/aS9BVRf331fLnq2Mpi76mt6G+/Z9xtI3lqyMbeqwVnxkK4ECr/e4xlT+3lGX42CWQBTi/Zr5ZwGa0QZbz46z1Rk6aPzh1jNoFJHl8sPgHdkg9HPB2st9KiIQ8NDWhCHODMpho+oLLUARwjw6OPU3Qfx45CiJ9ubzBTIt6/VFu4jnwTe69M67piqh7ySpXDQOjlYFit/ivJ/YUWh6rl6RS3zf8VZkLDAl"
+  access_key = "ASIARHZXYLTZ6R7HYHMA"
+  secret_key = "PSPl9hcufdYWvREDjqfFA2tyebrxTE701BgXrgIT"
+  token      = "FwoGZXIvYXdzEHoaDGWKfWp4duKIetAt0SLBAVCszasTsyJxTUS3tlr2B8wZJGsKuXGyKlgYrQ5IScfR2IMgUqNJaK5J3W/Wk46t4a5zvVMjkn1qdGYcP/0Zzm/1O1tkj9dEvJPf+72DfdJCeK0xO5RkkRuiOiVtqOTbbvlcVTOf9Te7U156pbrSMeCbC8jTkdIEiuhLahUN+T2fVmdHFzKY1rLavbBPgqir7K1EIIMe5u0GvT1rL5ZvFzBuRhCmUrkEba4oWVS41lUBiy7it6sA/IqImgZp7DA9ApQo/Ofo8wUyLYRx09mA92iWI+mz6tg+zIrvTKoFnKyRPEaj8jdwQ69NDRHBMCXRY3IR6kpKbg=="
 }
