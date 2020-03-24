fcda8221985dcefe03979bb8e35960b4f8bef11a
Ubuntu <ubuntu@ip-172-31-95-12.ec2.internal>
2020-03-24 17:43:12 +0000

    nuevas keys

--- provider.tf
+++ provider.tf
@@ -1,6 +1,6 @@
 provider "aws" {
   region     = "us-east-1"
-  access_key = "ASIARHZXYLTZ6R7HYHMA"
-  secret_key = "PSPl9hcufdYWvREDjqfFA2tyebrxTE701BgXrgIT"
-  token      = "FwoGZXIvYXdzEHoaDGWKfWp4duKIetAt0SLBAVCszasTsyJxTUS3tlr2B8wZJGsKuXGyKlgYrQ5IScfR2IMgUqNJaK5J3W/Wk46t4a5zvVMjkn1qdGYcP/0Zzm/1O1tkj9dEvJPf+72DfdJCeK0xO5RkkRuiOiVtqOTbbvlcVTOf9Te7U156pbrSMeCbC8jTkdIEiuhLahUN+T2fVmdHFzKY1rLavbBPgqir7K1EIIMe5u0GvT1rL5ZvFzBuRhCmUrkEba4oWVS41lUBiy7it6sA/IqImgZp7DA9ApQo/Ofo8wUyLYRx09mA92iWI+mz6tg+zIrvTKoFnKyRPEaj8jdwQ69NDRHBMCXRY3IR6kpKbg=="
+  access_key = "ACCESSKEY"
+  secret_key = "SECRETKEY"
+  token      = "SESSIONTOKEN"
 }
