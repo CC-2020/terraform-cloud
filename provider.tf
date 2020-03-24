f240e0797789ea012160b55cc285d9b311b6d3b2
J.L. Álvarez <alvarez@uhu.es>
2020-03-24 08:48:06 +0000

    Actualizar Token de Sesion

--- provider.tf
+++ provider.tf
@@ -2,4 +2,5 @@ provider "aws" {
   region     = "us-east-1"
   access_key = "ASIASA7D5YWLSEGG3UZ6"
   secret_key = "ZRbQFLYEYWICpiVEHfoDylZJkegfN0bKQdFfZ/G4"
+  token      = "FwoGZXIvYXdzEHEaDGem8obr+SZwlQPU2yK9AeG/+2rd6Ol/u88hhHOcfVsZl6ch2WFmp4W+cMboaZmVgx2i12APAE1JZ/Ad0+IFDcDuVEEFESM976psKGM51e5awR4tjcOR5np/aS9BVRf331fLnq2Mpi76mt6G+/Z9xtI3lqyMbeqwVnxkK4ECr/e4xlT+3lGX42CWQBTi/Zr5ZwGa0QZbz46z1Rk6aPzh1jNoFJHl8sPgHdkg9HPB2st9KiIQ8NDWhCHODMpho+oLLUARwjw6OPU3Qfx45CiJ9ubzBTIt6/VFu4jnwTe69M67piqh7ySpXDQOjlYFit/ivJ/YUWh6rl6RS3zf8VZkLDAl"
 }
